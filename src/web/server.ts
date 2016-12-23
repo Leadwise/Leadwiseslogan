cf834bdc14268e9ef4434966d19aafa5d8cbb92c
Maciej Makowski <maciejm.github@cfiet.net>
2016-12-23 12:30:49 +0000

    Slogan rendering

--- src/web/server.ts
+++ src/web/server.ts
@@ -1,18 +1,46 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { Observable } from 'rxjs';
 import * as express from 'express';
+import * as expressWinston from 'express-winston';
 import createLogger from '../common/logging';
+import * as mustacheExpress from 'mustache-express';
+import { sample } from 'lodash';
 
 const logger = createLogger('web', 'server');
 const app = express();
 
+const viewsDir = path.join(__dirname, '..', '..', 'views');
+const assetsDir = path.join(__dirname, '..', '..', 'assets');
+
 import { setupDatabase, ConnectionFactory } from '../common/database';
 
+const fsReadDir = Observable.bindNodeCallback<string, string[]>(fs.readdir);
+
 export async function startWebServer(dbOpen: ConnectionFactory, port: number = 80) {
-  //app.use('/static', express.static('web-resources'));
+  logger.info('Using following directories', { viewsDir, assetsDir });
+
+  app.engine('mustache', mustacheExpress());
+  app.set('view engine', 'mustache');
+  app.set('views', viewsDir);
+
+  app.use("/assets", express.static(assetsDir));
 
   app.get('/', (req: express.Request, res: express.Response) => {
     res.redirect('/slogan');
     res.end();
-  })
+  });
+
+  app.use(expressWinston.logger({
+    winstonInstance: logger
+  }));
+
+  const backgrounds = await fsReadDir('assets').map(files => 
+    files.filter(f => 
+      f.startsWith('propaganda') && f.endsWith('jpg')
+    )
+  ).toPromise();
+  logger.info(`Found ${backgrounds.length} avaliable backgrounds`);
 
   app.get('/slogan', async (req: express.Request, res: express.Response) => {
     let dbConnection = await dbOpen();
@@ -50,8 +78,12 @@ export async function startWebServer(dbOpen: ConnectionFactory, port: number = 8
         res.writeHead(404, `Not Found: Could not find slogan with id ${sloganId}`);
         return res.end();
       }
-      res.json(sloganRow);
-      return res.end();
+
+      return res.render("index", {
+        sloganKey: sloganRow.key,
+        slogan: sloganRow.text,
+        bg: sample(backgrounds)
+      });
     } finally {
       dbConnection.done();
     }
